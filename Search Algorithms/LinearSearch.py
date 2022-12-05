00d50a8b66cfedd951c63e8d85e74cf40aaa6dc9
sirvin01 <91463820+sirvin01@users.noreply.github.com>
2022-12-05 11:59:20 +0300

    Update LinearSearch.py

--- Search Algorithms/LinearSearch.py
+++ Search Algorithms/LinearSearch.py
@@ -1,12 +1,20 @@
-#Finding the maximum value using linear search
+# In this program, Linear search is implimented in two ways:Finding the maximum value in an array, Finding a specific element
 def linearsearch(n,my_arr,max_value):
     for i in range(0,n):
         if my_arr[i] > max_value:
             max_value = my_arr[i]
         index_value = my_arr.index(max_value)    
     print("The maximum value is {} at index {}".format(max_value,index_value))
-
-# my_arr = [3,4,9,10,6,7,8]
+def linearsearch1(n,my_arr):
+    value = int(input("Enter a number to search for:"))
+    for i in range(0,n):
+        if my_arr[i] == value:
+            value = my_arr[i]
+        index_value = my_arr.index(value)    
+        print("{} was found at index {}".format(value,index_value))
+        break
+    else:
+        print("{} not found!".format(value))
 
 l = int(input("Enter the number of elements in your array:"))
 print("Enter the elements of your array and press enter after each element:")
@@ -18,6 +26,15 @@ for i in range(0,l):
 
 max_value = my_arr[0]
 n = len(my_arr)
-linearsearch(n,my_arr,max_value)
+print("1.Find a specific element 2.Find the maximum value")
+choice = int(input("Enter an operation to perform: 1 or 2:"))
+
+if choice == 1:
+    linearsearch1(n,my_arr)
+elif choice == 2:
+    linearsearch(n,my_arr,max_value)
+else:
+    print("Please enter either 1 or 2")
+
 
     
