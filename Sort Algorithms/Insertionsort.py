f69fd84c3fb61aa213bf26c59f55df2cbb40c1e9
vinn65 <salimjr201@gmail.com>
2022-12-06 18:46:36 +0300

    Update Insertionsort.py

--- Sort Algorithms/Insertionsort.py
+++ Sort Algorithms/Insertionsort.py
@@ -1,3 +1,13 @@
+def Insertionsort(my_arr):
+    for i in range(1,n):
+        key = my_arr[i]
+        j = i - 1
+
+        while j >= 0 and key < my_arr[j]:
+            my_arr[j + 1] = my_arr[j]
+            j = j - 1
+        my_arr[j + 1] = key
+
 
 l = int(input("Enter the number of elements in your array:"))
 print("Enter the elements of your array and press enter after each element:")
@@ -9,4 +19,5 @@ for i in range(0, l):
     
 n = len(my_arr)
 print("Your unsorted list: ", my_arr)
-print("Your sorted list:",Bucketsort(my_arr))
+Insertionsort(my_arr)
+print("Your sorted list:",my_arr)
