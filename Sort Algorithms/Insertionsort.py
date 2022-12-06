c29f2ebdabc6755a1688b9a96741fd07e56c2b5b
vinn65 <salimjr201@gmail.com>
2022-12-06 18:13:27 +0300

    Update Insertionsort.py

---
+++ Sort Algorithms/Insertionsort.py
@@ -0,0 +1,12 @@
+
+l = int(input("Enter the number of elements in your array:"))
+print("Enter the elements of your array and press enter after each element:")
+
+my_arr = []
+for i in range(0, l):
+    item = int(input())
+    my_arr.append(item)
+    
+n = len(my_arr)
+print("Your unsorted list: ", my_arr)
+print("Your sorted list:",Bucketsort(my_arr))
