0a16d6b2c5b4023d4c2f2e05d686e99e2305f0a4
vinn65 <salimjr201@gmail.com>
2022-12-06 17:54:11 +0300

    Update Bucketsort.py

--- Sort Algorithms/Bucketsort.py
+++ Sort Algorithms/Bucketsort.py
@@ -1,9 +1,15 @@
 #Explanation done here -- https://stackabuse.com/bucket-sort-in-python/
 
-
-def Bucketsort(my_arr):
-
-    
+#Insertion is another type of sort algorithms. Here it`s used to sort elements in bucket
+def Insertionsort(bucket):
+    for i in range (1, len(bucket)):
+        var = bucket[i]
+        j = i - 1
+        while (j >= 0 and var < bucket[j]):
+            bucket[j + 1] = bucket[j]
+            j = j - 1
+        bucket[j + 1] = var
+   
 
 l = int(input("Enter the number of elements in your array:"))
 print("Enter the elements of your array and press enter after each element:")
