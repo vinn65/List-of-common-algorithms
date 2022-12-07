0af50522d00ce40377a233298d5d6d00fe1d44c3
vinn65 <salimjr201@gmail.com>
2022-12-07 22:15:03 +0300

    Update Countingsort.py

--- Sort Algorithms/Countingsort.py
+++ Sort Algorithms/Countingsort.py
@@ -1,23 +1,19 @@
 #Explanation done here -- https://favtutor.com/blogs/counting-sort-python
 def Countingsort(my_arr):
-    n = len(my_arr)
     output = [0] * n
     count = [0] * 10
-    # storing the count of each element 
-    for m in range(0, n):
-        count[my_arr[m]] += 1
-    # storing the cumulative count
-    for m in range(1, 10):
-        count[m] += count[m - 1]
-    # place the elements in output my_array after finding the index of each element of original my_array in count my_array
-    m = n - 1
-    while m >= 0:
-        output[count[my_arr[m]] - 1] = my_arr[m]
-        count[my_arr[m]] -= 1
-        m -= 1
+    for i in range(0, n):
+        count[my_arr[i]] += 1
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    i = n - 1
+    while i >= 0:
+        output[count[my_arr[i]] - 1] = my_arr[i]
+        count[my_arr[i]] -= 1
+        i -= 1
 
-    for m in range(0, n):
-        my_arr[m] = output[m]
+    for i in range(0, n):
+        my_arr[i] = output[i]
 
 
 
@@ -31,5 +27,5 @@ for i in range(0, l):
     
 n = len(my_arr)
 print("Your unsorted list: ", my_arr)
-Bubblesort(my_arr)
+Countingsort(my_arr)
 print("Your sorted list:",my_arr)
